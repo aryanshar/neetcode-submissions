d74b2e6108b69a31f8f61678507efd535215097a
Aryan Sharma <aryan.sharma575@gmail.com>
2026-09-13 22:16:31 +0530

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x:(x[0], x[1]))
+        visited = []
+        for start, end in intervals:
+            if not visited:
+                visited.append([start,end])
+            if(start<=visited[-1][1] or start==visited[-1][0]):
+                visited[-1][1] = max(end, visited[-1][1]) 
+            else:
+                visited.append([start, end])
+        return visited
